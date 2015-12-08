efaa007edbe6f98a52a315fbd71696509226b4e2
tomcat <tomcat.cheng@microlife.com.tw>
2015-12-08 15:12:02 +0800

    refine layout drink menu

---
+++ app/src/main/java/example/com/simpleui/DrinkMenuActivity.java
@@ -0,0 +1,15 @@
+package example.com.simpleui;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class DrinkMenuActivity extends AppCompatActivity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_drink_menu);
+
+    }
+}
--- app/src/main/java/example/com/simpleui/Utils.java
+++ app/src/main/java/example/com/simpleui/Utils.java
@@ -41,7 +41,7 @@ public class Utils {
         {
             e.printStackTrace();
         }
-        return null;
+        return "";
     }
 }
 
