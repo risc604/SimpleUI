73d6aff6c27a5ee43ffa02a5aa3ab4835885f5dc
tomcat <tomcat.cheng@microlife.com.tw>
2015-12-09 11:08:13 +0800

    Implement add(), add  cancel & down button

--- app/src/main/java/example/com/simpleui/DrinkMenuActivity.java
+++ app/src/main/java/example/com/simpleui/DrinkMenuActivity.java
@@ -2,6 +2,8 @@ package example.com.simpleui;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class DrinkMenuActivity extends AppCompatActivity
 {
@@ -10,6 +12,13 @@ public class DrinkMenuActivity extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_drink_menu);
+    }
 
+    public void add(View view)
+    {
+        Button  button = (Button) view;
+        int number = Integer.parseInt(button.getText().toString());
+        number++;
+        button.setText(String.valueOf(number));
     }
 }
