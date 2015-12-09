c512e3a3a4e528ff7e9af8216b247a0d54095808
tomcat <tomcat.cheng@microlife.com.tw>
2015-12-09 11:34:56 +0800

    use JSONObject and JSONArray to store menu status

--- app/src/main/java/example/com/simpleui/DrinkMenuActivity.java
+++ app/src/main/java/example/com/simpleui/DrinkMenuActivity.java
@@ -7,6 +7,10 @@ import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class DrinkMenuActivity extends AppCompatActivity
 {
     @Override
@@ -29,12 +33,14 @@ public class DrinkMenuActivity extends AppCompatActivity
         {"name":"milk tea", "1":10, "m":10},
         {"name":"green tea", "1":5, "m":10} ]
      */
-
-    public void getData()
+    public JSONArray getData()
     {
         LinearLayout    rooLinearLayout = (LinearLayout) findViewById(R.id.root);
-        int count = rooLinearLayout.getChildCount();
-        for (int i=0; i<(count-1); i++) {
+        int             count = rooLinearLayout.getChildCount();
+        JSONArray       array = new JSONArray();
+
+        for (int i=0; i<(count-1); i++)
+        {
             LinearLayout ll = (LinearLayout) rooLinearLayout.getChildAt(i);
 
             TextView drinkNameTextView = (TextView) ll.getChildAt(0);
@@ -44,8 +50,22 @@ public class DrinkMenuActivity extends AppCompatActivity
             String drinkName = drinkNameTextView.getText().toString();
             int lNumber = Integer.parseInt(lButton.getText().toString());
             int mNumber = Integer.parseInt(mButton.getText().toString());
+
+            try
+            {
+                JSONObject  object = new JSONObject();
+                object.put("name", drinkName);
+                object.put("l", lNumber);
+                object.put("m", mNumber);
+                array.put(object);
+            }
+            catch (JSONException e)
+            {
+                e.printStackTrace();
+            }
         }
 
+        return array;
     }
 }
 
