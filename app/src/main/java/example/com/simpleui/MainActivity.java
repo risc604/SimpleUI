82a155e52c45d009e13bd4686a1bfeb7b1d43398
tomcat <tomcat.cheng@microlife.com.tw>
2015-12-11 14:06:33 +0800

    Use SimpleAdapter, Setup Parse SDK, Store order information to Parse

--- app/src/main/java/example/com/simpleui/MainActivity.java
+++ app/src/main/java/example/com/simpleui/MainActivity.java
@@ -12,50 +12,72 @@ import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.SimpleAdapter;
 import android.widget.Spinner;
+
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.parse.Parse;
+import com.parse.ParseObject;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+
+
 public class MainActivity extends AppCompatActivity
 {
     private final static int REQUEST_CODE_MENU_ACTIVITY = 1;
-    private EditText    inputText;
+    private EditText inputText;
     //private Button      btnNext;
-    private CheckBox    hideCheckBox;
-    private SharedPreferences   sharedPreferences;
-    private SharedPreferences.Editor    editor;
-    private ListView    historyListView;
-    private Spinner     storeInfoSpinner;
-    private String      menuResult;
+    private CheckBox hideCheckBox;
+    private SharedPreferences sharedPreferences;
+    private SharedPreferences.Editor editor;
+    private ListView historyListView;
+    private Spinner storeInfoSpinner;
+    private String menuResult;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        Parse.enableLocalDatastore(this);
+        Parse.initialize(this);
+
+        //ParseObject testObject = new ParseObject("People");
+        //testObject.put("name", "Tom");
+        //testObject.put("age", "23");
+        //testObject.saveInBackground();
+
         setContentView(R.layout.activity_main);
 
         storeInfoSpinner = (Spinner) findViewById(R.id.storeInfoSpinner);
         sharedPreferences = getSharedPreferences("settings", Context.MODE_PRIVATE);
         editor = sharedPreferences.edit();
 
-        inputText = (EditText)findViewById(R.id.inputText);
+        inputText = (EditText) findViewById(R.id.inputText);
         //btnNext = (Button)findViewById(R.id.button);
-        hideCheckBox = (CheckBox)findViewById(R.id.hideCheckBox);
+       // hideCheckBox = (CheckBox) findViewById(R.id.hideCheckBox);
         //hideCheckBox.setChecked(true);
 
         //inputText.setText("1234");
         inputText.setText(sharedPreferences.getString("inputText", ""));
-        inputText.setOnKeyListener(new View.OnKeyListener()
-        {
+        inputText.setOnKeyListener(new View.OnKeyListener() {
             @Override
-            public boolean onKey(View v, int keyCode, KeyEvent event)
-            {
-                if(event.getAction() == KeyEvent.ACTION_DOWN)
-                {
-                    if(keyCode == KeyEvent.KEYCODE_ENTER)
-                    {
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                if (event.getAction() == KeyEvent.ACTION_DOWN) {
+                    if (keyCode == KeyEvent.KEYCODE_ENTER) {
                         submit(v);
                         return true;
                     }
@@ -65,20 +87,21 @@ public class MainActivity extends AppCompatActivity
         });
         //btnNext.setText("Next");
         hideCheckBox = (CheckBox) findViewById(R.id.hideCheckBox);
-        hideCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener()
-                        {
-                            @Override
-                            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked)
-                            {
-                                editor.putBoolean("hideCheckBox", isChecked);
-                                editor.commit();
-                            }
-                        });
+        hideCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                editor.putBoolean("hideCheckBox", isChecked);
+                editor.commit();
+            }
+        });
         hideCheckBox.setChecked(sharedPreferences.getBoolean("hideCheckBox", false));
 
         historyListView = (ListView) findViewById(R.id.historyListView);
         setHistory();
         setStoreInfo();
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
     private void setStoreInfo()
@@ -88,36 +111,71 @@ public class MainActivity extends AppCompatActivity
         storeInfoSpinner.setAdapter(storeAdapter);
     }
 
-    private void setHistory()
-    {
+    private void setHistory() {
         //String[] data = new String[] {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10"};
-        String[] data = Utils.readFile(this, "history.txt").split("\n");
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, data);
+        String[] rawData = Utils.readFile(this, "history.txt").split("\n");
+
+        // "name" -> "Tom"
+        // "birth" -> "19000909"
+        // "Sex" -> "M"
+        List<Map<String, String>> data = new ArrayList<>();
+
+        for (int i=0; i<rawData.length; i++)
+        {
+            try
+            {
+                JSONObject object = new JSONObject(rawData[i]);
+                String note = object.getString("note");
+                JSONArray array = object.getJSONArray("menu");
+
+                Map<String, String> item = new HashMap<>();
+                item.put("note", note);
+                item.put("drinkNum", "15");
+                item.put("storeInfo", "NTU Store");
+
+                data.add(item);
+            }
+            catch (JSONException e)
+            {
+                e.printStackTrace();
+            }
+       }
+
+        String[] from = {"note", "drinkNum", "storeInfo"};
+        int[] to = {R.id.note, R.id.drinkNum, R.id.storeInfo};
+        SimpleAdapter adapter = new SimpleAdapter(this, data, R.layout.listview_item, from, to);
+
+
+        //ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, data);
         historyListView.setAdapter(adapter);
     }
 
     public void submit(View view)       // button on Click action function.
     {
-        String text = inputText.getText().toString() + "," + menuResult;
+        String text = inputText.getText().toString() ;
         editor.putString("inputText", text);
         editor.commit();    // write to sharePreferce.
 
-        try
-        {
+        try {
             JSONObject orderData = new JSONObject();
-            JSONArray   array = new JSONArray(menuResult);
+            if (menuResult == null)
+                menuResult = "[]";
+            JSONArray array = new JSONArray(menuResult);
             orderData.put("note", text);
             orderData.put("menu", array);
-            Utils.writeFile(this, "history.txt", text + "\n");
-        }
-        catch (JSONException e)
-        {
+            Utils.writeFile(this, "history.txt", orderData.toString() + "\n");
+
+            ParseObject testObject = new ParseObject("Order");
+            testObject.put("note", text);
+            testObject.put("menu", array);
+            testObject.saveInBackground();
+
+        } catch (JSONException e) {
             e.printStackTrace();
         }
 
 
-        if(hideCheckBox.isChecked())
-        {
+        if (hideCheckBox.isChecked()) {
             text = "*******************";
             inputText.setText("*******************");
         }
@@ -133,14 +191,15 @@ public class MainActivity extends AppCompatActivity
         //intent.setClass(this, DrinkMenuActivity.class);
         //startActivity(intent);
         startActivityForResult(new Intent().setClass(this, DrinkMenuActivity.class), REQUEST_CODE_MENU_ACTIVITY);
-
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data)
     {
-        if(requestCode == REQUEST_CODE_MENU_ACTIVITY) {
-            if (resultCode == RESULT_OK) {
+        if (requestCode == REQUEST_CODE_MENU_ACTIVITY)
+        {
+            if (resultCode == RESULT_OK)
+            {
                 //String result = data.getStringExtra("result");
                 //Log.d("debug: ", result);
                 menuResult = data.getStringExtra("result");
@@ -148,6 +207,4 @@ public class MainActivity extends AppCompatActivity
             //super.onActivityResult(REQUEST_CODE_MENU_ACTIVITY, RESULT_OK, data);
         }
     }
-
-
 }
